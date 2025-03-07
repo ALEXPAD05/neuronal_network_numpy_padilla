5a3f82d8be9242ab026ab49a49d13bf401ca31cd
ALEXPAD05 <alexpadilla0592@gmail.com>
2025-03-06 23:10:31 -0600

    final_updates

--- src/neuronal.py
+++ src/neuronal.py
@@ -1,112 +1,144 @@
+# Import necessary libraries
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_gaussian_quantiles
 
-# Creemos datasets desde cero - Para un ejemplo de clasificacion
-N = 1000
-gaussian_quantiles = make_gaussian_quantiles(
-	mean=None,
-	cov=0.1,
-	n_samples=N,
-	n_features=2,
-	n_classes=2,
-	shuffle=True,
-	random_state=None)
+def neuronal():
+    # Create datasets from scratch - For a classification example
+    N = 1000
+    gaussian_quantiles = make_gaussian_quantiles(
+        mean=None,
+        cov=0.1,
+        n_samples=N,
+        n_features=2,
+        n_classes=2,
+        shuffle=True,
+        random_state=None)
 
-X, Y = gaussian_quantiles
-Y = Y[:,np.newaxis]
+    # Separate features (X) and labels (Y)
+    X, Y = gaussian_quantiles
+    Y = Y[:, np.newaxis]  # Reshape Y to be a column vector
 
-print(X.shape)
-print(Y.shape)
+    # Print the shapes of X and Y
+    print(X.shape)
+    print(Y.shape)
 
-plt.scatter(X[:,0], X[:,1],c=Y, s=40, cmap=plt.cm.Spectral)
+    # Plot the dataset with colors based on labels
+    plt.scatter(X[:, 0], X[:, 1], c=Y, s=40, cmap=plt.cm.Spectral)
 
-# Funciones de activacion
+    # Activation functions
+    def sigmoid(x, derivate=False):
+        if derivate:
+            return np.exp(-x) / (np.exp(-x) + 1) ** 2  # Derivative of sigmoid
+        else:
+            return 1 / (1 + np.exp(-x))  # Sigmoid function
 
-def sigmoid(x, derivate=False):
-    if derivate:
-        return np.exp(-x)/(np.exp(-x)+1)**2
-    else:
-        return 1/(1+np.exp(-x))
+    def relu(x, derivate=False):
+        if derivate:
+            x[x <= 0] = 0  # Derivative of ReLU
+            x[x > 0] = 1
+            return x
+        else:
+            return np.maximum(0, x)  # ReLU function
 
-def relu(x, derivate=False):
-    if derivate:
-        x[x<=0]=0
-        x[x>0]=1
-        return x
-    else:
-        return np.maximum(0,x)
+    # Loss function
+    def mse(y, y_hat, derivate=False):
+        if derivate:
+            return (y_hat - y)  # Derivative of MSE
+        else:
+            return np.mean((y_hat - y) ** 2)  # Mean Squared Error
 
-# Funcion de perdida
-def mse(y, y_hat, derivate=False):
-    if derivate:
-        return (y_hat-y)
-    else:
-        return np.mean((y_hat-y)**2)
+    # Network structure: Initialize weights and biases
+    def initialize_parameters_deep(layers_dims):
+        parameters = {}
+        L = len(layers_dims)
+        for l in range(0, L - 1):
+            # Initialize weights randomly
+            parameters['W' + str(l + 1)] = (np.random.rand(
+                layers_dims[l], layers_dims[l + 1]) * 2) - 1
+            # Initialize biases randomly
+            parameters['b' + str(l + 1)] = (np.random.rand(1,
+                layers_dims[l + 1]) * 2) - 1
+        return parameters
 
-# Estructura de la red: asignar pesos y biases
+    # Training function
+    def train(x_data, learning_rate, params, training=True):
+        # Input data
+        params['A0'] = x_data
 
-def initialize_parameters_deep(layers_dims):
-    parameters = {}
-    L = len(layers_dims)
-    for l in range(0,L-1):
-        parameters['W'+str(l+1)] = (np.random.rand(
-                        layers_dims[l], layers_dims[l+1])*2)-1
-        parameters['b'+str(l+1)] = (np.random.rand(1,
-                        layers_dims[l+1])*2)-1
-    return parameters
+        # First layer
+        params['Z1'] = np.matmul(params['A0'], params['W1']) + params['b1']
+        params['A1'] = relu(params['Z1'])
 
+        # Second layer
+        params['Z2'] = np.matmul(params['A1'], params['W2']) + params['b2']
+        params['A2'] = relu(params['Z2'])
 
-def train(x_data, learning_rate, params, training=True):
+        # Third layer
+        params['Z3'] = np.matmul(params['A2'], params['W3']) + params['b3']
+        params['A3'] = sigmoid(params['Z3'])
 
-    params['A0']=x_data
+        output = params['A3']
 
-    # Primer capa
-    params['Z1']=np.matmul(params['A0'], params['W1'])+params['b1']
-    params['A1']=relu(params['Z1'])
+        if training:
+            # Backpropagation
+            # Weights in the last layer
+            params['dZ3'] = mse(Y, output, True) * sigmoid(params['A3'], True)
+            params['dW3'] = np.matmul(params['A2'].T, params['dZ3'])
 
-    # Segunda capa
-    params['Z2']=np.matmul(params['A1'], params['W2'])+params['b2']
-    params['A2']=relu(params['Z2'])
+            # Weights in the second-to-last layer
+            params['dZ2'] = np.matmul(params['dZ3'], params['W3'].T) * relu(params['A2'], True)
+            params['dW2'] = np.matmul(params['A1'].T, params['dZ2'])
 
-    # Tercer capa
-    params['Z3']=np.matmul(params['A2'], params['W3'])+params['b3']
-    params['A3']=sigmoid(params['Z3'])
+            # Weights in the first layer
+            params['dZ1'] = np.matmul(params['dZ2'], params['W2'].T) * relu(params['A1'], True)
+            params['dW1'] = np.matmul(params['A0'].T, params['dZ1'])
 
-    output = params['A3']
+            # Gradient descent algorithm
+            params['W3'] = params['W3'] - params['dW3'] * learning_rate
+            params['W2'] = params['W2'] - params['dW2'] * learning_rate
+            params['W1'] = params['W1'] - params['dW1'] * learning_rate
 
-    if training:
-        # Backpropagation
-        # Pesos en la última capa
-        params['dZ3'] = mse(Y,output,True)*sigmoid(params['A3'],True)
-        params['dW3']=np.matmul(params['A2'].T, params['dZ3'])
+            params['b3'] = params['b3'] - (np.mean(params['dW3'], axis=0, keepdims=True)) * learning_rate
+            params['b2'] = params['b2'] - (np.mean(params['dW2'], axis=0, keepdims=True)) * learning_rate
+            params['b1'] = params['b1'] - (np.mean(params['dW1'], axis=0, keepdims=True)) * learning_rate
 
-        # Pesos en la penúltima capa
-        params['dZ2']=np.matmul(params['dZ3'], params['W3'].T)*relu(params['A2'],True)
-        params['dW2']=np.matmul(params['A1'].T, params['dZ2'])
+        return output
 
-        # Pesos de la primer capa
-        params['dZ1']=np.matmul(params['dZ2'], params['W2'].T)*relu(params['A1'], True)
-        params['dW1']=np.matmul(params['A0'].T, params['dZ1'])
+    # Define the architecture of the neural network
+    layers_dims = [2, 6, 10, 1]
+    params = initialize_parameters_deep(layers_dims)
+    errors = []
 
-        ## Algoritmo del gradiente descendiente
-        params['W3']=params['W3']-params['dW3']*learning_rate
-        params['W2']=params['W2']-params['dW2']*learning_rate
-        params['W1']=params['W1']-params['dW1']*learning_rate
+    # Training loop
+    for _ in range(50000):
+        output = train(X, 0.001, params)
+        if _ % 50 == 0:
+            print(mse(Y, output))  # Print the loss every 50 iterations
+            errors.append(mse(Y, output))  # Store the loss for plotting
 
-        params['b3']=params['b3']-(np.mean(params['dW3'], axis=0, keepdims=True))*learning_rate
-        params['b2']=params['b2']-(np.mean(params['dW2'], axis=0, keepdims=True))*learning_rate
-        params['b1']=params['b1']-(np.mean(params['dW1'], axis=0, keepdims=True))*learning_rate
+# Plot training errors
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(errors)
+    plt.title("Training Error Over Time")
+    plt.xlabel("Epochs/Iterations")
+    plt.ylabel("Error/Loss")
 
-    return output
+# Test the model on new data
+    data_test_x = (np.random.rand(1000, 2) * 2) - 1
+    data_test_y = train(data_test_x, 0.0001, params, training=False)
 
+# Convert predictions to binary values (0 or 1)
+    y = np.where(data_test_y > 0.5, 1, 0)
 
-layers_dims = [2, 6, 10, 1]
-params = initialize_parameters_deep(layers_dims) 
-error = []
+# Plot the test data with predicted labels
+    plt.subplot(1, 2, 2)
+    plt.scatter(data_test_x[:, 0], data_test_x[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+    plt.title("Test Data with Predicted Labels")
+    plt.xlabel("Feature 1")
+    plt.ylabel("Feature 2")
 
-for _ in range(50000):
-    output = train(X, 0.001, params)
-    if _%50 == 0:
-        print(mse(Y,output))
-        error.append(mse(Y,output))
+# Show all plots
+    plt.tight_layout()
+    plt.show()
